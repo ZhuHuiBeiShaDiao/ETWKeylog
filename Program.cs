1f5096dc44fe314a651a0660593663b4f9942037
Shaun Tame <shaun.tame@mixtel.com>
2016-10-28 08:10:48 +0200

    Initial check-in

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ObserveUSBEvents.StartTrace();
+		}
+	}
+}
